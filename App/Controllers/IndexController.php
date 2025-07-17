1e5f0fdff45b824b8fe28e9f22c0e8ca4d7ab41f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-17 12:32:39 -0300

    Definindo ações para o IndexController

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController {
+    public function index() {
+        echo 'Chegamos ao Index Controller e disparamos a action index';
+    }
+
+    public function sobre() {
+        echo 'Chegamos ao Index Controller e disparamos a action sobre';
+    }
+}
