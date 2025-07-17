fb151b4e1d5a8ded874849e351916e7f452f8dc5
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-17 12:32:15 -0300

    Criando métodos para as rotas e os controllers

--- App/Route.php
+++ App/Route.php
@@ -3,6 +3,25 @@
 namespace App;
 
 class Route {
+
+    private $routes;
+
+    public function __construct()
+    {
+        $this->initRoutes();
+        $this->run($this->getUrl());
+    }
+
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    public function setRoutes($atributo)
+    {
+        $this->routes = $atributo;
+    }
+
     public function initRoutes() {
         $routes['home'] = array(
             'route' => '/',
@@ -10,12 +29,27 @@ class Route {
             'action' => 'index'
         );
         $routes['sobre'] = array(
-            'route' => '/',
+            'route' => '/sobre',
             'controller' => 'indexController',
             'action' => 'sobre'
         );
+        
+        $this->setRoutes($routes);
     }
+
+    public function run($url) {
+        foreach ($this->getRoutes() as $key => $route) {
+            if ($url == $route['route']) {
+                $class = "App\\Controllers\\".ucfirst($route['controller']);
+                $controller = new $class;
+                $action = $route['action'];
+                $controller->$action();
+            }
+        }
+    }
+
     public function getUrl() {
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
+
 }
