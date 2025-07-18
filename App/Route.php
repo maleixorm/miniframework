cb59e52d71813aa8ca4c0a541379245d7c7893ed
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-18 15:45:01 -0300

    Definindo o método abstrato initRoutes

--- App/Route.php
+++ App/Route.php
@@ -1,28 +1,11 @@
 <?php
 
 namespace App;
+use MF\Init\Bootstrap;
 
-class Route {
+class Route extends Bootstrap{
 
-    private $routes;
-
-    public function __construct()
-    {
-        $this->initRoutes();
-        $this->run($this->getUrl());
-    }
-
-    public function getRoutes()
-    {
-        return $this->routes;
-    }
-
-    public function setRoutes($atributo)
-    {
-        $this->routes = $atributo;
-    }
-
-    public function initRoutes() {
+    protected function initRoutes() {
         $routes['home'] = array(
             'route' => '/',
             'controller' => 'indexController',
@@ -35,21 +18,6 @@ class Route {
         );
         
         $this->setRoutes($routes);
-    }
-
-    public function run($url) {
-        foreach ($this->getRoutes() as $key => $route) {
-            if ($url == $route['route']) {
-                $class = "App\\Controllers\\".ucfirst($route['controller']);
-                $controller = new $class;
-                $action = $route['action'];
-                $controller->$action();
-            }
-        }
-    }
-
-    public function getUrl() {
-        return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 
 }
