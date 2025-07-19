1c643724b25d8cbdcb63fbfe675fdd6ad1fa56de
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-19 10:44:43 -0300

    Criando atributos e funções para o controller da index

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -3,11 +3,28 @@
 namespace App\Controllers;
 
 class IndexController {
+    
+    private $view;
+
+    public function __construct()
+    {
+        $this->view = new \stdClass();
+    }
+    
     public function index() {
-        echo 'Chegamos ao Index Controller e disparamos a action index';
+        $this->view->dados = ['Sofá', 'Cadeira', 'Cama'];
+        $this->render('index');
     }
 
     public function sobre() {
-        echo 'Chegamos ao Index Controller e disparamos a action sobre';
+        $this->view->dados = ['Macbook', 'iPad', 'iPhone'];
+        $this->render('sobre');
+    }
+
+    public function render($view) {
+        $classeAtual = get_class($this);
+        $classeAtual = str_replace('App\\Controllers\\', '', $classeAtual);
+        $classeAtual = strtolower(str_replace('Controller', '', $classeAtual));
+        require_once "../App/Views/".$classeAtual."/".$view.".phtml";
     }
 }
