05bbe6ff7228c03d3420665f49532e4269be7def
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-19 15:24:41 -0300

    Criando a função content e modificando a função render

--- vendor/MF/Controller/Action.php
+++ vendor/MF/Controller/Action.php
@@ -11,11 +11,22 @@ abstract class Action {
         $this->view = new \stdClass();
     }
 
-    protected function render($view)
+    protected function render($view, $layout)
+    {
+        $this->view->page = $view;
+        if (file_exists("../App/Views/".$layout.".phtml")) {
+            require_once "../App/Views/".$layout.".phtml";
+        } else {
+            $this->content();
+        }
+        
+    }
+
+    protected function content()
     {
         $classeAtual = get_class($this);
         $classeAtual = str_replace('App\\Controllers\\', '', $classeAtual);
         $classeAtual = strtolower(str_replace('Controller', '', $classeAtual));
-        require_once "../App/Views/".$classeAtual."/".$view.".phtml";
+        require_once "../App/Views/".$classeAtual."/".$this->view->page.".phtml";
     }
 }
