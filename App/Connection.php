c1563f18939f2fdb4748f402882bdfc855424501
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-20 20:34:09 -0300

    Modificando para estática a função getDb

--- App/Connection.php
+++ App/Connection.php
@@ -7,7 +7,7 @@ use PDO;
 use PDOException;
 
 class Connection {
-    public function getDb() {
+    public static function getDb() {
         try {
             $conn = new PDO("mysql:host=localhost;dbname=mvc;charset=utf8", "php", "123456");
             return $conn;
